e5d6b8f50f24e825716586e2953f40b42200c619
Kellyxia0217 <114585624+Kellyxia0217@users.noreply.github.com>
2022-10-11 08:25:49 -0400

    Update TipCalcRunner.java

--- src/TipCalcRunner.java
+++ src/TipCalcRunner.java
@@ -2,26 +2,29 @@ import java.util.*;
 public class TipCalcRunner {
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
-        int numPeople;
-        int TipPercent;
-        double TotalBillBeforeTip = 0;
-
-
-
-
-
         System.out.println("Welcome to the Tip Calculator! " );
+
+
         System.out.print("How many people are in your group? " );
-        numPeople = input.nextInt();
+        int numPeople = input.nextInt();
         System.out.print("What is the Tip Percent? ");
-        TipPercent = input.nextInt();
-        while (input.nextDouble() > -1) {
+        int TipPercent = input.nextInt();
+        TipCalc order = new TipCalc(numPeople, TipPercent);
+        double MealCost = 0;
+        while (!(MealCost == -1)) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): " );
-            TotalBillBeforeTip =+ input.nextDouble();
+             MealCost = input.nextDouble();
+            TipCalc.addMeal(MealCost);
 
         }
         System.out.println("-------------------------");
-        System.out.print("Total Bill Before Tip: " );
+        System.out.printf("Total Bill Before Tip: $%.2f\n", TipCalc.getTotalBillBeforeTip());
+        System.out.println("Tip Percentage: "+ TipCalc.getTipPercentage());
+        System.out.printf("Total Tip: $%.2f\n", TipCalc.tipAmount());
+        System.out.printf("Total Bill With Tip: $%.2f\n", TipCalc.totalBill());
+        System.out.printf("Per Person Cost Before Tip: $%.2f\n", TipCalc. perPersonCostBeforeTip());
+        System.out.printf("Tip Per Person: $%.2f\n", TipCalc.perPersonTipAmount());
+        System.out.printf("Total Cost Per Person: $%.2f\n", TipCalc.perPersonTotalCost());
 
 
 
